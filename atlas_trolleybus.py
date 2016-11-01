8ded2c763e1f1e936b9668f22dd66e2e5f81a62e
trolleway <trolleway@yandex.ru>
2016-11-02 01:36:27 +0300

    generate big map

--- atlas_trolleybus.py
+++ atlas_trolleybus.py
@@ -6,6 +6,7 @@ import psycopg2
 import time
 import config
 import argparse
+import urllib
 
 def argparser_prepare():
 
@@ -37,15 +38,29 @@ def render_atlas(host,dbname,user,password):
         return 0
     cur = conn.cursor()
 
-        
+
+
+
+    #Получаем одну картинку со всеми слоями на всю карту
+
+    #Генерируем к этой картинке файл привязки
+    #Запрашиваем охваты отдельных страниц атласа
+    #Режем картинку на страницы по географическим координатам в EPSG:3857 используя gdal
+    #Собираем из страниц книжку
+
+    
     tmpfiles=dict()
     tmpfiles['lines'] = 'tmp/lines.png'
     tmpfiles['terminals'] = 'tmp/terminals.png'
 
     tmpfiles['stage1'] = 'tmp/stage1.tif'
     tmpfiles['background'] = 'tmp/background.png'
+
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
     #Query for active maps
     
+    #Запрашиваем охват листа с названием all
     cmd='''
 ogr2ogr -f PostgreSQL "PG:host='''+host+''' dbname='''+dbname+''' user='''+user+''' password='''+password+'''" cfg/atlaspages.geojson -nln atlaspages -progress -overwrite    \
 
@@ -53,12 +68,7 @@ ogr2ogr -f PostgreSQL "PG:host='''+host+''' dbname='''+dbname+''' user='''+user+
     print cmd
     os.system(cmd)
     cur.execute('''
-SELECT CONCAT(
-ST_YMin(Box2D(wkb_geometry)),',',
-ST_XMin(Box2D(wkb_geometry)),',',
-ST_YMax(Box2D(wkb_geometry)),',',
-ST_XMax(Box2D(wkb_geometry))
-) AS bbox_string,
+SELECT 
 CONCAT(
 ST_XMin(Box2D(ST_Transform(wkb_geometry,3857))),' ',
 ST_YMax(Box2D(ST_Transform(wkb_geometry,3857))),' ',
@@ -81,11 +91,50 @@ ST_XMax(ST_Transform(wkb_geometry,3857)) - ST_XMin(ST_Transform(wkb_geometry,385
 )::real AS aspect
 ,ref,map
 FROM atlaspages
-WHERE map='mostrans-frequent-atlas4'
+WHERE map='mostrans-frequent-atlas4' AND ref='all'
 ORDER BY map,ref;
                 ''')
     rows = cur.fetchall()
     for currentmap in rows:
+        print currentmap#['map_id']
+
+        size=7500
+
+        url="http://trolleway.nextgis.com/api/component/render/image?resource=715,725&extent="+str(currentmap[1])+"&size="+str(size)+","+str(int(round(size*float(currentmap[2]))))
+        print url
+
+        urllib.urlretrieve (url, tmpfiles['lines'])
+
+
+        cur.execute('''
+SELECT 
+CONCAT(
+ST_XMin(Box2D(ST_Transform(wkb_geometry,3857))),' ',
+ST_YMax(Box2D(ST_Transform(wkb_geometry,3857))),' ',
+ST_XMax(Box2D(ST_Transform(wkb_geometry,3857))),' ',
+ST_YMin(Box2D(ST_Transform(wkb_geometry,3857)))
+) AS bbox_string_gdal
+,
+CONCAT(
+ST_XMin(Box2D(ST_Transform(wkb_geometry,3857))),',',
+ST_YMin(Box2D(ST_Transform(wkb_geometry,3857))),',',
+ST_XMax(Box2D(ST_Transform(wkb_geometry,3857))),',',
+ST_YMax(Box2D(ST_Transform(wkb_geometry,3857)))
+) AS bbox_string_ngw_image
+,
+(
+ST_YMax(ST_Transform(wkb_geometry,3857)) - ST_YMin(ST_Transform(wkb_geometry,3857))
+)/
+(
+ST_XMax(ST_Transform(wkb_geometry,3857)) - ST_XMin(ST_Transform(wkb_geometry,3857))
+)::real AS aspect
+,ref,map
+FROM atlaspages
+WHERE map='mostrans-frequent-atlas4' AND ref='all'
+ORDER BY map,ref;
+                ''')
+        rows = cur.fetchall()
+        for currentmap in rows:
         print currentmap#['map_id']
 
         continue
