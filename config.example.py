7a07dab6e56e0c27a1740a5f14bac6aff35c451f
Artem Svetlov <trolleway@yandex.ru>
2016-11-14 22:26:09 +0300

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -7,4 +7,4 @@ ngw_url='http://.nextgis.com'
 ngw_login = ''
 ngw_password = '' 
 
-#obtain yandex token from https://oauth.yandex.ru/authorize?response_type=code&client_id=<идентификатор приложения
+#obtain yandex token from https://oauth.yandex.ru/authorize?response_type=token&client_id=<идентификатор приложения
