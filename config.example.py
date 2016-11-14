fe57262d47f1569203cf5c645aae94e075403602
Artem Svetlov <trolleway@yandex.ru>
2016-11-14 22:24:31 +0300

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -6,3 +6,5 @@ password='localgisuserpassword'
 ngw_url='http://.nextgis.com'
 ngw_login = ''
 ngw_password = '' 
+
+#obtain yandex token from https://oauth.yandex.ru/authorize?response_type=code&client_id=<идентификатор приложения
