6a94c7eece17614cc87d294576baf09b7d8680a8
Artem Svetlov <trolleway@yandex.ru>
2016-11-24 22:41:44 +0300

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -10,6 +10,7 @@ ngw_url='http://.nextgis.com'
 ngw_login = ''
 ngw_password = '' 
 
-#obtain yandex token from https://oauth.yandex.ru/authorize?response_type=token&client_id=<идентификатор приложения
+#register new yandex.disk app at https://oauth.yandex.ru/
+#obtain yandex token from https://oauth.yandex.ru/authorize?response_type=token&client_id=<идентификатор приложения>
 yandex_token = ''
 yandex_disk_path = 'Приложения/osm-mostrans'
