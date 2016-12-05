b088918a3c1d9958225d11965310d3c95105ed76
Artem Svetlov <trolleway@yandex.ru>
2016-12-05 21:35:42 +0300

    change filenames for yandex

--- mostrans-trolleybus-atlas.py
+++ mostrans-trolleybus-atlas.py
@@ -104,8 +104,9 @@ def render_atlas(host,dbname,user,password):
 
     now = datetime.datetime.now()
 
-    mapname='Московский троллейбус'
-    longname = 'Москва, атлас троллейбусных маршрутов'
+    mapname = 'Московский троллейбус'
+    longname_single = 'Москва, карта троллейбусных маршрутов'
+    longname_atlas = 'Москва, атлас троллейбусных маршрутов'
     tmpfiles=dict()   
     tmpfiles['folder'] = 'tmp'
 
@@ -258,17 +259,16 @@ ORDER BY map,ref;
     os.system(cmd)
 
     print 'Upload png to Yandex'
-    upload_yandex(config.yandex_token,pathdata=dict(path=config.yandex_disk_path + longname + ' [Openstreetmap] [latest].png',overwrite='True'),filedata=open(tmpfiles['screenall']+'.png', 'rb'))
+    upload_yandex(config.yandex_token,pathdata=dict(path=config.yandex_disk_path + longname_single + ' [Openstreetmap] [latest].png',overwrite='True'),filedata=open(tmpfiles['screenall']+'.png', 'rb'))
 
     cmd="gdal_translate -of ""GTiff"" -a_srs ""EPSG:3857"" -co ""COMPRESS=DEFLATE"" -co ""ZLEVEL=9"" " + tmpfiles['screenall'] + ".png " + tmpfiles['screenall'] + '.tiff'
-    #gdal_translate -of "GTiff" -a_srs "EPSG:3857" -co "COMPRESS=JPEG" -co "JPEG_QUALITY=96" tmp/mostrans-trolleybus-atlas4-all-screen.png tmp/mostrans-trolleybus-atlas4-all-screen.tiff
     os.system(cmd)
     print 'Upload GeoTIF to Yandex'
-    upload_yandex(config.yandex_token,pathdata=dict(path=config.yandex_disk_path + longname + ' [Openstreetmap] [latest].tif',overwrite='True'),filedata=open(tmpfiles['screenall']+'.tiff', 'rb'))
+    upload_yandex(config.yandex_token,pathdata=dict(path=config.yandex_disk_path + longname_single + ' [Openstreetmap] [latest].tif',overwrite='True'),filedata=open(tmpfiles['screenall']+'.tiff', 'rb'))
     upload_yandex(config.yandex_token,pathdata=dict(path=config.yandex_disk_path + tmpfiles['atlas_yandex']+'.tif',overwrite='True'),filedata=open(tmpfiles['screenall']+'.tiff', 'rb'))
 
     print 'Upload PDF to Yandex'
-    upload_yandex(config.yandex_token,pathdata=dict(path=config.yandex_disk_path + longname + ' [Openstreetmap] [latest].pdf',overwrite='True'),filedata=open(tmpfiles['atlas'], 'rb'))
+    upload_yandex(config.yandex_token,pathdata=dict(path=config.yandex_disk_path + longname_atlas + ' [Openstreetmap] [latest].pdf',overwrite='True'),filedata=open(tmpfiles['atlas'], 'rb'))
 
     
 if __name__ == '__main__':
