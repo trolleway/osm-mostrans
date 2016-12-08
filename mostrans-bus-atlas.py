e4a7a64c10dd83601504bd7a83deca9d51b07ba7
Artem Svetlov <trolleway@yandex.ru>
2016-12-09 00:39:29 +0300

    using wms instead of webmap rendering

--- mostrans-bus-atlas.py
+++ mostrans-bus-atlas.py
@@ -80,49 +80,6 @@ def upload_yandex(token,pathdata,filedata):
             if response.status_code <> 201:
                 print 'Error upload file to Yandex'
                 
-                
-def render_atlas(host,dbname,user,password):
-
-    size_main=9500
-    ConnectionString="dbname=" + dbname + " user="+ user + " host=" + host + " password=" + password
-    try:
-        conn = psycopg2.connect(ConnectionString)
-    except:
-        print 'I am unable to connect to the database                  ' 
-        print ConnectionString
-        return 0
-    cur = conn.cursor()
-
-    #debug
-    retrive_map=True
-
-    #Получаем одну картинку со всеми слоями на всю карту
-    #Генерируем к этой картинке файл привязки
-    #Запрашиваем охваты отдельных страниц атласа
-    #Режем картинку на страницы по географическим координатам в EPSG:3857 используя gdal
-    #Собираем из страниц книжку
-
-    now = datetime.datetime.now()
-
-    mapname='Московский автобус'
-    longname_atlas = 'Москва, атлас автобусных маршрутов'
-    longname_single = 'Москва, карта автобусных маршрутов'
-    tmpfiles=dict()   
-    tmpfiles['folder'] = 'tmp'
-
-    tmpfiles['screenall']=os.path.join(tmpfiles['folder'], "mostrans-bus-all-screen") #no extension
-    tmpfiles['atlas'] = 'tmp/moscow_bus_ru_openstreetmap_'+now.strftime("%Y-%m-%d")+'.pdf'
-    tmpfiles['atlas_yandex'] = 'archive/moscow_bus_ru_openstreetmap_'+now.strftime("%Y-%m-%d") #withouth time - only one file at day will saved
-
-    if not os.path.exists('tmp'):
-        os.makedirs('tmp')
-
-    #Load to PostGIS config file with pages bounds
-    cmd='''
-ogr2ogr -f PostgreSQL "PG:host='''+host+''' dbname='''+dbname+''' user='''+user+''' password='''+password+'''" cfg/mostrans-bus-atlas.geojson -nln atlaspages  -overwrite'''
-    os.system(cmd)
-  
-
     def ngw2png(where,ngwstyles,size,filename):
         #if size is big, then image will be retrieved from wfs service
         cur.execute('''
@@ -259,10 +216,51 @@ def wfs2png(where,size,filename,wfs_url='',wfslayers=''):
             worldfile.close()
         return
     
+    
+    
+def render_atlas(host,dbname,user,password):
 
+    size_main=9500
+    ConnectionString="dbname=" + dbname + " user="+ user + " host=" + host + " password=" + password
+    try:
+        conn = psycopg2.connect(ConnectionString)
+    except:
+        print 'I am unable to connect to the database                  ' 
+        print ConnectionString
+        return 0
+    cur = conn.cursor()
 
+    #debug
+    retrive_map=True
 
-#Согласно принципу KISS: генерируются одиночные pdf в gdal, затем они склеиваются в один посредством imagemagick
+    #Получаем одну картинку со всеми слоями на всю карту
+    #Генерируем к этой картинке файл привязки
+    #Запрашиваем охваты отдельных страниц атласа
+    #Режем картинку на страницы по географическим координатам в EPSG:3857 используя gdal
+    #Собираем из страниц книжку
+
+    now = datetime.datetime.now()
+
+    mapname='Московский автобус'
+    longname_atlas = 'Москва, атлас автобусных маршрутов'
+    longname_single = 'Москва, карта автобусных маршрутов'
+    tmpfiles=dict()   
+    tmpfiles['folder'] = 'tmp'
+
+    tmpfiles['screenall']=os.path.join(tmpfiles['folder'], "mostrans-bus-all-screen") #no extension
+    tmpfiles['atlas'] = 'tmp/moscow_bus_ru_openstreetmap_'+now.strftime("%Y-%m-%d")+'.pdf'
+    tmpfiles['atlas_yandex'] = 'archive/moscow_bus_ru_openstreetmap_'+now.strftime("%Y-%m-%d") #withouth time - only one file at day will saved
+
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+
+    #Load to PostGIS config file with pages bounds
+    cmd='''
+ogr2ogr -f PostgreSQL "PG:host='''+host+''' dbname='''+dbname+''' user='''+user+''' password='''+password+'''" cfg/mostrans-bus-atlas.geojson -nln atlaspages  -overwrite'''
+    os.system(cmd)
+  
+
+    #Согласно принципу KISS: генерируются одиночные pdf в gdal, затем они склеиваются в один посредством imagemagick
     atlaspages=list()
     cur.execute('''
 SELECT 
