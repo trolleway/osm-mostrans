0d7afcbeb96534cfc73b0ac1c90e641eb9220d51
Artem Svetlov <trolleway@yandex.ru>
2019-06-29 12:17:25 +0200

    Update atlas-get.py

--- osm-atlas-get/atlas-get.py
+++ osm-atlas-get/atlas-get.py
@@ -1,14 +1,35 @@
 
+#import configuration gpkg to postgis
+
+#pages query
+
 for page in result:
     #prepare poly file for clip
+    
     polyfile = os.path.join(datadir,'roi.poly')
     cmd = 'ogr2ogr -where "map_id={map_id}" '
     cmd = cmd.format(map_id=page['map_id'])
+    print cmd
+    os.system(cmd)
+    
+    cmd = 'ogr2poly.py'
+    cmd = cmd.format
+    print cmd
+    os.system(cmd)
     
     #prepare pbf file for lines
-    cmd = 'python process_pbf.py --filter "{filter}" --poly "{polyfile}" --source "{source_pbf}" --result "{result_pbf}" '
-    cmd = cmd.format(filter = page['filter'], polyfile=polyfile, source_pbf = os.path.join('datadir','source.pbf')
     
+    #clip pbf file by poly
+    cmd = 'osmconvert polyfile country_pbf city_pbf'
+    cmd = cmd.format(polyfile=polyfile)
+    print cmd
+    os.system(cmd)
+    
+    #filter routes from pbf
+    cmd = 'python filter_pbf.py --filter "{filter}"  --source "{source_pbf}" --result "{result_pbf}" '
+    cmd = cmd.format(filter = page['filter'],  source_pbf = os.path.join('datadir','city_pbf.pbf')
+    print cmd
+    os.system(cmd)   
     #prepare pbf file for basemap
     
     #import pbf to postgis
